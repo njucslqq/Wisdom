efd375836975f3ac2dfcdaf7121416813590196d
njucslqq <njucslqq@gmail.com>
2016-01-13 20:40:30 +0800

    jchart

--- src/main/java/tongtong/qiangqiang/research/Research.java
+++ src/main/java/tongtong/qiangqiang/research/Research.java
@@ -1,26 +1,28 @@
 package tongtong.qiangqiang.research;
 
 
+import cn.quanttech.quantera.common.data.BarInfo;
+import cn.quanttech.quantera.common.data.TickInfo;
+import cn.quanttech.quantera.common.data.TimeFrame;
 import jwave.Transform;
 import jwave.transforms.FastWaveletTransform;
 import jwave.transforms.wavelets.daubechies.Daubechies3;
-import jwave.transforms.wavelets.haar.Haar1;
-import weka.clusterers.SimpleKMeans;
-import weka.core.Instance;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.Minute;
+import org.jfree.data.time.TimeSeries;
+import tongtong.qiangqiang.data.H;
+import tongtong.qiangqiang.vis.TimeSeriesChart;
 
-import java.io.File;
+import java.sql.Date;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import static cn.quanttech.quantera.CONST.INTRA_QUANDIS_URL;
-import static cn.quanttech.quantera.CONST.OUTRA_QUANDIS_URL;
 import static cn.quanttech.quantera.datacenter.DataCenterUtil.setNetDomain;
 import static java.time.LocalDate.of;
-import static tongtong.qiangqiang.data.H.ticks;
 import static tongtong.qiangqiang.func.Util.extract;
-import static tongtong.qiangqiang.research.Filter.lowPass;
 import static tongtong.qiangqiang.research.Filter.lowPassFilter;
 
 
@@ -45,18 +47,23 @@ public class Research {
 
     public static final int TOP = 4;
 
+    public static final String FMT = "MM-dd HH-mm-ss";
+
     public static void main(String[] args) {
         setNetDomain(INTRA_QUANDIS_URL);
+
+        TimeSeriesChart tscB = new TimeSeriesChart("Time Series");
+        TimeSeriesChart tscA = new TimeSeriesChart("Time Series");
 
         int sample = 10;
         String code = "IF1601";
         LocalDate date = of(2015, 12, 22);
-        List<Double> price = extract(ticks(code, date), "lastPrice");
-        price.remove(0);
-        int span = price.size() / sample;
+        List<BarInfo> tickList = H.bars(code, TimeFrame.MIN_1, date.minusDays(20), date.plusDays(30));
+        tickList.remove(0);
+        List<Double> price = extract(tickList, "closePrice");
 
-        int index = 50;
-        for (; index < price.size(); index += span) {
+        int index = 0;
+        for (; index < price.size(); index++) {
             List<Double> data = new ArrayList<>();
             if (index + 1 < HEAD) {
                 int count = HEAD - (index + 1);
@@ -66,14 +73,27 @@ public class Research {
                 data.addAll(price.subList(index + 1 - HEAD, index + 1));
             for (int j = 0; j < TAIL; j++)
                 data.add(price.get(index));
+
             Transform t = new Transform(new FastWaveletTransform(new Daubechies3()));
             List<Double> res = lowPassFilter(t, data, TOP);
-            File file = new File(BASE + index + "[" + TOP + "]" + FILE);
-            FileEcho echo = new FileEcho(file.getAbsolutePath());
-            int i = HEAD > (index + 1) ? HEAD - index - 1 : 0;
-            for (; i < HEAD; i++)
-                echo.writeln(data.get(i), res.get(i));
-            echo.close();
+
+            TimeSeries before = new TimeSeries("before");
+            TimeSeries after = new TimeSeries("after");
+            int i = HEAD > (index + 1) ? 0 : index + 1 - HEAD;
+            int j = HEAD > (index + 1) ? HEAD - index - 1 : 0;
+            for (; i <= index; i++,j++) {
+                before.addOrUpdate(new Minute(Date.from(tickList.get(i).tradingTime.atZone(ZoneId.systemDefault()).toInstant())), price.get(i));
+                after.addOrUpdate(new Minute(Date.from(tickList.get(i).tradingTime.atZone(ZoneId.systemDefault()).toInstant())), res.get(j));
+            }
+
+            tscB.vis(FMT, before);
+            tscA.vis(FMT, after);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ src/main/java/tongtong/qiangqiang/vis/TimeSeriesChart.java
@@ -0,0 +1,91 @@
+package tongtong.qiangqiang.vis;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RectangleInsets;
+import org.jfree.ui.RefineryUtilities;
+
+import java.awt.*;
+import java.text.SimpleDateFormat;
+
+/**
+ * Author: Qiangqiang Li
+ * <p>
+ * Coding is another way to meet the curiousness of a curious mind
+ * <p>
+ * 2016-01-13.
+ */
+public class TimeSeriesChart extends ApplicationFrame {
+
+    static {
+        ChartFactory.setChartTheme(new StandardChartTheme("JFree/Shadow", true));
+    }
+
+    public TimeSeriesChart(String title) {
+        super(title);
+    }
+
+    public void vis(String fmt, TimeSeries... series) {
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        for (TimeSeries ts : series)
+            dataset.addSeries(ts);
+
+        JFreeChart chart = createChart(dataset, fmt);
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setFillZoomRectangle(true);
+        panel.setMouseWheelEnabled(true);
+        panel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(panel);
+
+        pack();
+        //RefineryUtilities.centerFrameOnScreen(this);
+        setVisible(true);
+    }
+
+    private JFreeChart createChart(XYDataset dataset, String fmt) {
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Time Series Data",  // title
+                "Time",             // x-axis label
+                "Price",   // y-axis label
+                dataset,            // data
+                true,               // create legend?
+                true,               // generate tooltips?
+                false               // generate URLs?
+        );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYLineAndShapeRenderer) {
+            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+            renderer.setBaseShapesVisible(true);
+            renderer.setBaseShapesFilled(true);
+            renderer.setDrawSeriesLineAsPath(true);
+        }
+
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat(fmt));
+
+        return chart;
+
+    }
+}
