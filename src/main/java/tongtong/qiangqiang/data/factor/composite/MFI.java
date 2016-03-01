d750b6949c503cbeda0ec3585bbf16a2f08747d8
njucslqq <njucslqq@gmail.com>
2016-03-01 17:50:48 +0800

    add two volume indicators

--- src/main/java/tongtong/qiangqiang/data/factor/composite/MFI.java
+++ src/main/java/tongtong/qiangqiang/data/factor/composite/MFI.java
@@ -17,15 +17,18 @@ import static java.lang.Integer.MAX_VALUE;
  */
 public class MFI extends WIN<BarInfo> {
 
-    public final Intermediate mf, pmf, nmf, mfi;
+    public final Intermediate tp, pmf, nmf, mfi;
 
-    public MFI() {
-        this(MAX_VALUE);
+    public final int period;
+
+    public MFI(int period) {
+        this(period, MAX_VALUE);
     }
 
-    public MFI(int cacheSize) {
-        super(1);
-        mf = new Intermediate(cacheSize);
+    public MFI(int period, int cacheSize) {
+        super(0);
+        this.period = period;
+        tp = new Intermediate(cacheSize);
         pmf = new Intermediate(cacheSize);
         nmf = new Intermediate(cacheSize);
         mfi = new Intermediate(cacheSize);
@@ -38,8 +41,28 @@ public class MFI extends WIN<BarInfo> {
 
     @Override
     public double update(BarInfo input) {
-        double tp = (input.highPrice + input.lowPrice + input.closePrice) / 3.;
-        return 0;
+        double _tp = (input.highPrice + input.lowPrice + input.closePrice) / 3.;
+        if (tp.dataSize() == 0) {
+            tp.update(_tp);
+            pmf.update(0.0);
+            nmf.update(0.0);
+            mfi.update(0.0);
+        } else {
+            double _mf = _tp * input.volume;
+            pmf.update(_tp > tp.data.last(0) ? _mf : 0.0);
+            nmf.update(_tp < tp.data.last(0) ? _mf : 0.0);
+            double _pmf = 0.0, _nmf = 0.0;
+            if (pmf.dataSize() < period) {
+                _pmf = pmf.data.all().stream().mapToDouble(d -> d).sum();
+                _nmf = nmf.data.all().stream().mapToDouble(d -> d).sum();
+            } else {
+                _pmf = pmf.last(period).stream().mapToDouble(d -> d).sum();
+                _nmf = nmf.last(period).stream().mapToDouble(d -> d).sum();
+            }
+            mfi.update(_pmf / (_pmf + _nmf));
+            tp.update(_tp);
+        }
+        return mfi.data.last(0);
     }
 
     @Override
